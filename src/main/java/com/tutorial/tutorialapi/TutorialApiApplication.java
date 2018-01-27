cd606821231a156eaaf07cccb0809821b331c46b
Rahul Kadam <rkadam@corp.qualys.com>
2018-01-27 19:48:49 +0530

    adding zul changes

--- src/main/java/com/tutorial/tutorialapi/TutorialApiApplication.java
+++ src/main/java/com/tutorial/tutorialapi/TutorialApiApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import com.tutorial.tutorialapi.Repository.*;
 import org.springframework.boot.actuate.health.Health;
 
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TutorialApiApplication {
 
--- src/main/java/com/tutorial/tutorialapi/controller/UserController.java
+++ src/main/java/com/tutorial/tutorialapi/controller/UserController.java
@@ -4,13 +4,14 @@ import com.tutorial.tutorialapi.Repository.UserRepository;
 import com.tutorial.tutorialapi.dao.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
+@RefreshScope
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -32,6 +33,16 @@ public class UserController {
         return userList;  */
     }
 
+    @GetMapping("/users11")
+    public Collection<User> getUserList() {
+        return userRepository.findAll();
+        /*
+        List<String> userList = new ArrayList<>();
+        userList.add("Mission Impossible - 2016");
+        userList.add("Mission Need to start - 2017");
+        userList.add("Mission is started again - 2018");
+        return userList;  */
+    }
     @PostMapping("/create")
     public String createUser(@Valid String userName) {
         userRepository.save(new User(userName));
